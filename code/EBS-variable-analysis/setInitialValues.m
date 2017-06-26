aeca232beb1b58cf1ee0b0373a124da3a7386bf7
Orfeas <orfeasa@gmail.com>
2017-06-26 22:55:19 +0300

    Work on chapter 4, variable analysis

---
+++ code/EBS-variable-analysis/setInitialValues.m
@@ -0,0 +1,63 @@
+%% Constants
+global r_e; % electron radius
+r_e = 2.8179403267e-15; % in m
+global c;   % speed of light
+c = 299792458; % in m/s
+global e;   % electron charge
+e = 1.602176565e-19;
+global E_0;
+E_0 = 0.510998910e+6; %in eV
+global q_e;
+q_e = 1.602176565e-19; %in Coulomb
+
+%% Initial Variables
+% accelerated voltage
+V = 20e3; %volts
+
+% offset in Y axis 
+rho = 0.01; % in m
+
+% bunch length
+sigma_s = 4e-3; % in m
+
+% beta: relative velocity of the testing beam, beta = v_t / c
+gamma = 1 + V / E_0;
+beta = sqrt(1 - 1 / (gamma ^ 2));
+
+% charge per pulse
+Q_i = 5.9e-4; % in Coulomb, 590 uC
+
+% bunches per pulse
+N_b = 70128;
+
+% electrons per bunch
+N_e = Q_i / (N_b * q_e);
+
+% n(z): relativistic bunch linear density along Z
+sigma = sigma_s; 
+n = @(z) N_e * normpdf(z, 0, sigma);
+
+%% Values for variable ranges
+
+% The resolution of the plots is specified here by the number of
+% samples
+x_acc = 500;
+y_acc = x_acc;
+
+
+xmin = -0.5;
+xmax = 0.5;
+x = linspace(xmin, xmax, x_acc);
+
+Q_i_min = 5.9e-5;
+Q_i_max = 5.9e-3;
+
+sigma_min = 0.1e-3;
+sigma_max = 5e-3;
+
+rho_min = 0;
+rho_max = 2;
+
+V_min = 2e2; %was 20e2
+V_max = 2e6; %was 20e4
+
---
+++ code/EBS-variable-analysis/staticBeamDeflection.m
@@ -0,0 +1,31 @@
+function [theta_y, theta_z] = staticBeamDeflection( ...
+	x, rho, beta, n, r_e, sigma)
+%STATICBEAMDEFLECTION Calculates the deflection angle of the 
+% testing beam
+% x:	position of the scanning beam, assuming the main beam is 
+%	   centered at x = 0	   
+% rho:  offset of the scanning beam in Y axis
+% beta: relative velocity of the testing beam, beta = v_t / c
+% n(z): function expressing the main beam linear density along 
+%	   Z axis
+% r_e:  electron radius
+% sigma:bunch length
+	  
+	% compute theta Y
+	fun_y = @(z) n(z) ./ (rho .* rho + (x + beta .* z) .* ...
+		(x + beta .* z));
+	%Integral_y = integral(fun_y, -Inf, Inf);
+	Integral_y = integral(fun_y, -3 * sigma, 3 * sigma);
+	theta_y = 2 * rho * r_e / beta * Integral_y;
+
+	
+	% compute theta Z
+	fun_z = @(z) ((x + beta .* z) .* n(z)) ./ (...
+        rho .* rho + (x + beta .* z) .* (x + beta .* z));
+	
+	%Integral_z = integral(fun_z, -Inf, Inf);
+	Integral_z = integral(fun_z, -3 * sigma, 3 * sigma);
+	theta_z = 2 * r_e * Integral_z;
+
+end
+
